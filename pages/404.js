5c9e6dc143625f50381b9a58b661eb3effc198ab
Abakhri <s.abakhri@softpro.com>
2022-08-01 01:07:45 +0300

    404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+function Page404() {
+    return (
+        <div className={styles.container}>
+            <h1>Page not Found!111</h1>
+        </div>
+    )
+}
+
+export default Page404;
