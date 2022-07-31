5ab4edb680d52aa64b329407881dade4bd1b93c6
Abakhri <s.abakhri@softpro.com>
2022-07-31 15:56:38 +0300

    programmatically navigation

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,11 +1,22 @@
 import {useRouter} from "next/router";
+import {useCallback} from "react";
 
 function ClientProjectsPage() {
     const router = useRouter();
+    const loadProject = useCallback(() => {
+        router.push({
+            pathname: '/clients/[id]/[projectId]',
+            query: {
+                id: 'samir',
+                projectId: 'Project A'
+            }
+        })
+    }, []);
 
   return (
     <div>
         <h1>Client projects page for {router.query.id}</h1>
+        <button onClick={loadProject}>Load a project</button>
     </div>
   )
 }
